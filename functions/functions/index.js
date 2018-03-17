689a55ce39a2f744d2b2f46129b430d149a665b9
or <orshapira91@gmail.com>
2018-03-17 18:43:43 +0200

    final

---
+++ functions/functions/index.js
@@ -0,0 +1,438 @@
+const utils         = require('./utils');
+const utilsWeb      = require('./utilsWeb');
+const utilsStrategy = require('./utilsStrategy');
+const firebase      = require("firebase");
+const functions     = require('firebase-functions');
+const moment        = require("moment-timezone");
+const rp            = require('request-promise');
+const async         = require('asyncawait/async');
+const await         = require('asyncawait/await');
+firebase.initializeApp({
+    apiKey: "AIzaSyAPxVY9M579OhCfjHPTP834q7w4xPiLLns",
+    authDomain: "shapira-pro.firebaseapp.com",
+    databaseURL: "https://shapira-pro.firebaseio.com",
+    projectId: "shapira-pro",
+    storageBucket: "shapira-pro.appspot.com",
+    messagingSenderId: "813284272810"
+});
+ 
+var database = firebase.database();
+var cachedSharesData;
+var cachedEarningsCalendar;
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// firebase ruls
+// {
+//  "rules": {
+//    ".read": "auth != null",
+//    ".write": "auth != null"
+//  }
+//}
+//
+
+var getDailyReturns = async(function(date) {
+    var wantedDate = utils.clearFormatedTZDate(moment.tz(date, "America/New_York")).substring(0,10);
+    var paramsValues = [];
+    var arrInfo = [];
+    var updates = {};
+    for(var key in utils.params) paramsValues.push(utils.params[key]);
+    var paramsPath = paramsValues.join('/');
+    var dbPath = '/eaSymbolsToBuy/' + paramsPath + '/' + wantedDate;
+    var snapshot = await (database.ref(dbPath).once('value'));
+    
+    if (snapshot.exists()) {
+        var symbolsToBuy = snapshot.val();
+        var keys = Object.keys(symbolsToBuy);
+        for (var k = 0; k < keys.length; k++) {
+            var currElement = symbolsToBuy[keys[k]];
+            currElement.data = await (getPositionReturns(currElement.symbol, wantedDate, updates))
+            arrInfo.push(currElement);
+        }
+    }
+
+    database.ref().update(updates);
+    return arrInfo;
+});
+
+var getPositionReturns = async(function(symbol, date, updates) {
+    var wantedDate = utils.clearFormatedTZDate(moment.tz(date, "America/New_York")).substring(0,10);
+    var dbPath = '/eaPositionsInfo/' + wantedDate + '/' + symbol.replace('.','-');
+    
+    // if there are values in db return them! 
+    var snapshot = await (database.ref(dbPath).once('value'));
+
+    if (snapshot.exists())
+        return (snapshot.val());
+    else {
+        var dataStart = await (utilsWeb.getYahooShareDataBeforeWantedDate(symbol, 1, wantedDate, true, false));
+        var dataEnd = await (utilsWeb.getYahooShareDataBeforeWantedDate(symbol, 1, wantedDate, false, true));
+        
+        if (dataStart.length == 1 && dataEnd.length == 1) {
+            
+            var positionInfo = { 
+                positionReturn: (dataEnd[0].close / dataStart[0].close), 
+                open: dataStart[0].close, 
+                close: dataEnd[0].close, 
+                low: dataEnd[0].low,
+                high: dataEnd[0].high,
+                dateOpen : dataStart[0].date, 
+                dateClose : dataEnd[0].date, 
+            };
+
+            if (updates)
+                updates[dbPath] = positionInfo;
+            else {
+                var updates = {};
+                updates[dbPath] = positionInfo;
+                database.ref().update(updates);
+            }
+            
+            return (positionInfo);
+        }
+
+        return ({ positionReturn: 1, close: 1, open: 1 });
+    }
+});
+
+var predictInvestmentsByDate = async(function(date) {
+
+    var ET = utils.dateToCheck(date);
+    var updates = {};
+    var paramsValues = [];
+    for(var key in utils.params) paramsValues.push(utils.params[key]);       
+    var paramsPath = paramsValues.join('/');
+    var dbPath = '/eaSymbolsToBuy/' + paramsPath + '/' + ET.wantedTimeET.substring(0,10);
+
+    // Not a valid time
+    if (!ET.validTimeCheck) {
+        var now = moment.tz('America/New_York');
+        var notActionTime = { message: "Not a time for any action", timeET : moment.tz(ET.wantedTimeET, 'America/New_York').format('DD.MM.YYYY HH:mm') };
+
+        // Update DB only before decisions time 
+        if ((now.hour() < utils.decisionsTime.hour) || 
+            (now.hour() == utils.decisionsTime.hour && now.minute() < utils.decisionsTime.minute)) {
+            updates[dbPath] = notActionTime;
+            database.ref().update(updates);
+        }
+
+        return (notActionTime);  
+    }
+    else {
+
+        var arrEarningAnnouncements = [];
+
+        // if there are values in db return them! 
+        var snapshot = await (database.ref(dbPath).once('value'));
+        var earnings = snapshot.val();
+
+        if (earnings && !earnings.message)
+            arrEarningAnnouncements = earnings;
+        else {
+            arrEarningAnnouncements = await (getEarningsCalendar(ET.wantedTimeET));
+            arrEarningAnnouncements = await (addDataLayer(arrEarningAnnouncements, utils.params.windowSize, ET.wantedTimeET));
+            arrEarningAnnouncements = utilsStrategy.minimizeSharesList(arrEarningAnnouncements, utils.params);
+            arrEarningAnnouncements = utilsStrategy.addInvestmentDataLayer(arrEarningAnnouncements);
+            arrEarningAnnouncements.forEach(element => { if (element.data) delete element.data; });
+            updates[dbPath] = arrEarningAnnouncements;
+            database.ref().update(updates);
+        }
+
+        return (arrEarningAnnouncements);
+    }
+});
+
+var addDataLayer = async (function(arrEarningAnnouncements, nNumOfDays, wantedDate) {
+
+    var updates = {};
+    var asyncInChunk = 5;
+
+    //arrEarningAnnouncements.forEach(element => {
+    for (var index = 0; index < arrEarningAnnouncements.length; index+=asyncInChunk) {
+        
+        let chunk = arrEarningAnnouncements.slice(index,index+asyncInChunk)
+        await (Promise.all(chunk.map(async ((element) => {
+
+            try {
+                element.data = await (getShareDataBeforeWantedDate(element.symbol, nNumOfDays, wantedDate.substring(0,10), updates));
+            }
+            catch (err) {
+                var strErr = String(err);
+                if (strErr.indexOf("Our engineers") != -1)  {
+                    //console.log("We've been blocked!, they thing we did D-DOS-!-!-!-!-!-!-!-!-!-!-!-!")
+                }
+                else
+                    console.error(err);
+            }
+
+        }))));
+    }
+    //});
+
+    arrEarningAnnouncements.forEach(element => {if (!element.data) console.error(element.symbol + " didn't get DataLayer - " + wantedDate)});
+    database.ref().update(updates);
+    return arrEarningAnnouncements;
+});
+
+var getShareDataBeforeWantedDate = async (function(symbol, nNumOfDays, wantedDate, updates) {
+
+    var arrToReturn = [];
+    var dbPath = '/eaSharesData/' + symbol.replace('.','-');
+    var shareData;
+    var retriveDataFromWeb = true;
+    var index;
+
+    
+    if (cachedSharesData) {
+        shareData = cachedSharesData[symbol];
+    }
+    if (shareData) {
+        //console.log("Had " + symbol + " in Cashe!");
+    }
+    else {
+        var snapshot = await (database.ref(dbPath).once('value'));
+        shareData = snapshot.val();
+        cachedSharesData = {};
+        cachedSharesData[symbol] = shareData;
+    }
+
+    // If data exists
+    if (!shareData) 
+        shareData = [];
+    else {
+
+        shareData.sort((a, b) => { return moment(b.date).diff(moment(a.date)) });
+
+        // Find the wandetd date index
+        for (index = 0; index < shareData.length; index++) {
+            if (shareData[index].wantedDate && 
+                shareData[index].wantedDate.wantedDate == wantedDate) 
+                break;
+        }
+
+        // If data in DB contains all the transaction request - no need to retrive the data from the web
+        if ((index < shareData.length) &&
+            (shareData[index].wantedDate) &&
+            (shareData[index].wantedDate.wantedDate == wantedDate) && 
+            (shareData[index].wantedDate.plus >= nNumOfDays))
+            retriveDataFromWeb = false;
+    }
+
+    // If there is no need to retrive the data from the web
+    if (!retriveDataFromWeb) {
+        arrToReturn = shareData.slice(index, index + nNumOfDays );
+        //console.log("Had " + symbol + " relevant dates in DB!");
+    }
+    else {
+        var arrNewData = await (utilsWeb.getYahooShareDataBeforeWantedDate(symbol, nNumOfDays, wantedDate, true, false)); 
+
+        // Add the new data
+        arrNewData.forEach(element => {
+            shareData.push(element);
+        });
+
+        // Sort data by date (from new to old)
+        shareData.sort((a, b) => { return moment(b.date).diff(moment(a.date)) });
+
+        // Remove all duplicate data
+        for (var removeIndex = shareData.length - 2; removeIndex >= 0; removeIndex--) {
+            if (shareData[removeIndex].date == shareData[removeIndex + 1].date) {
+                var curr = shareData[removeIndex].wantedDate ? shareData[removeIndex].wantedDate.plus : 0;
+                var next = shareData[removeIndex + 1].wantedDate ? shareData[removeIndex + 1].wantedDate.plus : 0;
+                if (curr < next)
+                    shareData[removeIndex] = shareData[removeIndex + 1];
+                
+                shareData.splice(removeIndex + 1, 1);
+            }
+        }
+
+        arrToReturn = shareData;
+        updates[dbPath] = arrToReturn;
+        return (arrNewData);
+    }
+
+    return arrToReturn;
+})
+
+var getEarningsCalendar = async (function(wantedDate, isBatch) {
+
+    var arrToReturn = [];
+    var formatedWandedDate = wantedDate.substring(0,10);
+    var dbPath = '/eaCalendar/' + formatedWandedDate;
+    var snapshot;
+    var currEarningsCalendar;
+    
+    // If not a batch request
+    if (!isBatch) {
+        snapshot = await (database.ref(dbPath).once('value'));
+        if (snapshot.exists()) {
+            currEarningsCalendar = snapshot.val();
+            //console.log("Try take relevant EA-Calendar data form DB");
+        }
+    }
+    else {
+        if (!cachedEarningsCalendar) {
+            snapshot = await (database.ref('/eaCalendar/').once('value'));
+            cachedEarningsCalendar = snapshot.val();
+            //console.log("Take all EA-Calendar data for BATCH-REQUEST!");
+        }
+
+        if (cachedEarningsCalendar) 
+            currEarningsCalendar = cachedEarningsCalendar[formatedWandedDate];
+    }
+
+    // Check if exists in db
+    if (currEarningsCalendar) { 
+        arrToReturn = currEarningsCalendar;
+    }
+    else {
+
+        var sameDay = moment.tz(formatedWandedDate, "America/New_York");
+        var dayBefore = moment.tz(sameDay - (86400000), "America/New_York");
+        if (sameDay.day() == 6)
+            sameDay = moment.tz(moment(sameDay) + (86400000), "America/New_York");
+        if (sameDay.day() == 0)
+            sameDay = moment.tz(moment(sameDay) + (86400000), "America/New_York");
+        if (dayBefore.day() == 0)
+            dayBefore = moment.tz(moment(dayBefore) - (86400000), "America/New_York");
+        if (dayBefore.day() == 6)
+            dayBefore = moment.tz(moment(dayBefore) - (86400000), "America/New_York");
+
+        sameDay = utils.clearFormatedTZDate(sameDay);
+        dayBefore = utils.clearFormatedTZDate(dayBefore);
+        arrToReturn = await (utilsWeb.getZacksEarningsCalendar(dayBefore, "amc", arrToReturn));
+        arrToReturn = await (utilsWeb.getZacksEarningsCalendar(sameDay, "bmo", arrToReturn));
+
+        var updates = {};
+        updates[dbPath] = arrToReturn;
+        database.ref().update(updates);
+        //console.log("Took rlevant calendar from ZACKS-WEB and save it to DB!");
+    }
+
+    return arrToReturn;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exports.getDailyReturns = functions.https.onRequest(async ((fbReq, fbRes) => {
+
+    var { date } = fbReq.body;
+    if (!date)
+        res.sendStatus(400);
+
+    try {
+        fbRes.send(await(getDailyReturns(date)));
+    }
+    catch (err) {
+        console.error(err);
+        fbRes.sendStatus(400);
+    }
+}));
+exports.getPositionReturns = functions.https.onRequest(async ((fbReq, fbRes) => {
+
+    var { date, symbol } = fbReq.body;
+    if (!date || !symbol)
+        res.sendStatus(400);
+
+    try {
+        fbRes.send(await(getPositionReturns(symbol,date)));
+    }
+    catch (err) {
+        console.error(err);
+        fbRes.sendStatus(400);
+    }
+}));
+exports.predictInvestmentsByDate = functions.https.onRequest(async ((fbReq, fbRes) => {
+    var { date } = fbReq.body;
+    try {
+        fbRes.send(await(predictInvestmentsByDate(date)));
+    }
+    catch (err) {
+        console.error(err);
+        fbRes.sendStatus(400);
+    }
+}));
+
+
+
+
+
+
+
+
+
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 8080; 
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+var server = app.listen(port, function () {
+  console.log('EA - listening on port ' + port + '!');
+});
+server.setTimeout(600000);
+
+// // git push heroku master
+// // heroku logs -t
+// // herolu restart -a shapira
+
+var ams = 0;
+var ainvocations = 0;
+app.post('/getPositionReturns', async ((req, res) => {
+
+    var { date, symbol } = req.body;
+    if (!date || !symbol)
+        res.sendStatus(400);
+    try {
+        var start = (new Date()).getTime();
+        var result = await(getPositionReturns(symbol,date));
+        var end = (new Date()).getTime();
+        ams += (end - start);
+        ainvocations++;
+        var delta = ams / ainvocations;
+        console.log(date + ': getPositionReturns ' + delta + 'ms');
+        res.send(result);
+    }
+    catch (err) {
+        console.error(err);
+        res.sendStatus(400);
+    }
+}));
+
+var bms = 0;
+var binvocations = 0;
+app.post('/predictInvestmentsByDate', async ((req, res) => {
+
+    var { date } = req.body;
+    try {
+        var start = (new Date()).getTime();
+        var result = await(predictInvestmentsByDate(date));
+        var end = (new Date()).getTime();
+        bms += (end - start);
+        binvocations++;
+        var delta = bms / binvocations;
+        console.log(date + ': predictInvestmentsByDate ' + delta + 'ms');
+        res.send(result);
+    }
+    catch (err) {
+        console.error(err);
+        res.sendStatus(400);
+    }
+}));
+
+//predictInvestmentsByDate("2017-04-26")
+//getDailyReturns("2017-04-04")
+predictInvestmentsByDate("2017-08-03")
---
+++ functions/functions/utilsWeb.js
@@ -0,0 +1,193 @@
+var utils           = require('./utils');
+var moment          = require("moment-timezone");
+var rp              = require('request-promise');
+const async         = require('asyncawait/async');
+const await         = require('asyncawait/await');
+const herokuProxy   = 'https://shapira.herokuapp.com/getProxy';
+
+module.exports = {
+    
+    getYahooShareDataBeforeWantedDate : async (function(symbol, nNumOfDays, wantedDate, firstDayLessThan, firstDayGreaterOrEqual) {
+
+        const url = "https://finance.yahoo.com/quote/" + symbol + "/history?"
+        var arrToReturn = [];
+        var numOfTrys = 0;
+        var dayRows = [];
+        var strToFind = "\"HistoricalPriceStore\":{\"prices\":";
+        var a = "{\"date\":";
+        var b = "\"open\":";
+        var c = "\"close\":";
+        var d = "\"volume\":";
+        var e = "\"high\":";
+        var f = "\"low\":";
+
+        while (arrToReturn.length == 0 && numOfTrys < 3) {
+
+            var currDate    = utils.clearFormatedTZDate(moment.tz("2100-01-01", "America/New_York")).substring(0,10);
+            var startDate   = utils.clearFormatedTZDate(moment.tz("1970-01-01", "America/New_York")).substring(0,10);
+
+            try {
+
+                var html = await (rp({
+                    url : herokuProxy, 
+                    method: 'POST',  
+                    headers: {
+                        'Content-Type' : 'application/json',
+                        'Accept' : 'application/json',
+                    }, 
+                    body: JSON.stringify({url})}));
+                var nStartIndex = html.indexOf(strToFind);
+                if (nStartIndex == -1)
+                    throw "No data in YAHOO for - " + symbol;
+                html = html.substring(strToFind.length + nStartIndex);
+                var nEndIndex = html.indexOf(']');
+                if (nEndIndex == -1)
+                    throw "No data in YAHOO for - " + symbol;
+                html = html.substring(0, nEndIndex);
+                dayRows = html.split('}');
+
+                for (var nDayIndex = 0; nDayIndex < dayRows.length; nDayIndex++) {
+
+                    var arrValues;
+                    try {
+                        arrValues   = dayRows[nDayIndex].substring(1).split(',');
+                        var lDate   = parseInt(arrValues[0].substring(a.length));
+                        currDate    = utils.clearFormatedTZDate(moment.tz(moment(startDate).milliseconds() + (lDate * 1000), "America/New_York")).substring(0,10);
+                    }
+                    catch (err) { };
+
+                    if (currDate < wantedDate) {
+
+                        for (let nOffsetIndex = firstDayGreaterOrEqual ? 1 : 0; 
+                            ((firstDayLessThan) && (nOffsetIndex < nNumOfDays) && (nOffsetIndex + nDayIndex < dayRows.length - 1)) || 
+                            ((firstDayGreaterOrEqual) && (nOffsetIndex <= nNumOfDays) && (nDayIndex - nOffsetIndex >= 0)); 
+                            nOffsetIndex++) {
+
+                            try {
+
+                                let offsetDir   = firstDayGreaterOrEqual ? -1 : 1;
+                                arrValues       = dayRows[nDayIndex + (nOffsetIndex * offsetDir)].substring(1).split(',');
+                                let lcurrDate   = parseInt(arrValues[0].substring(a.length));
+                                let dOpen       = parseFloat(arrValues[1].substring(b.length));
+                                let dHigh       = parseFloat(arrValues[2].substring(e.length));
+                                let dLow        = parseFloat(arrValues[3].substring(f.length));
+                                let dClose      = parseFloat(arrValues[4].substring(c.length));
+                                let lVolume     = parseInt(arrValues[5].substring(d.length));
+                                currDate        = utils.clearFormatedTZDate(moment.tz(moment(startDate).milliseconds() + (lcurrDate * 1000), "America/New_York")).substring(0,10); 
+                                let selectedPlus = null;
+
+                                if (arrToReturn.length == 0) {
+                                    selectedPlus = { 
+                                        wantedDate : wantedDate,
+                                        plus : nNumOfDays,
+                                    }
+                                }
+
+                                if (isNaN(lcurrDate) || isNaN(dOpen) || isNaN(dHigh) || isNaN(dLow) || isNaN(dClose) || isNaN(lVolume))
+                                    nNumOfDays++;
+                                else 
+                                    arrToReturn.push({
+                                        wantedDate : selectedPlus,
+                                        date : currDate,
+                                        close : dClose, 
+                                        high : dHigh, 
+                                        low : dLow, 
+                                        open : dOpen, 
+                                        volume : lVolume,
+                                        symbol : symbol
+                                    });
+                            }
+                            catch (err) {
+                                if (dayRows[nOffsetIndex + nDayIndex].indexOf("DIVIDEND") == -1) {
+                                    //console.log(dayRows[nOffsetIndex + nDayIndex] + "\n" + err);
+                                }
+                                else
+                                    nNumOfDays++;
+                            }
+                        }
+
+                        break;
+                    }
+                }
+            }
+            catch (err) {
+                var strErr = String(err);
+                if (strErr.indexOf("Our engineers") != -1) {
+                    console.error("We've been blocked!, they think we did D-DOS-!-!-!-!-!-!-!-!-!-!-!-!")
+                }
+                else
+                    console.error(err);
+            }
+
+            numOfTrys++;
+            if (numOfTrys == 3 && arrToReturn.length == 0) {
+                //console.log(dayRows.length + " Lines for :    " + symbol); 
+            }
+        }
+
+        return arrToReturn;
+    }),
+
+    getZacksEarningsCalendar : async (function(selectedDate, indicator, arrToReturn) {
+
+        var containsSymbolsMap = {};
+        arrToReturn.forEach(element => containsSymbolsMap[element.symbol] = 1);
+        var startDate = moment.tz("1970-01-01", "America/New_York");
+
+        try {
+
+            var longDate = ((moment.tz(selectedDate, "America/New_York")) - startDate) / 1000 + 22000;
+            const url = "https://www.zacks.com/includes/classes/z2_class_calendarfunctions_data.php?calltype=eventscal&date=" + longDate + "&type=1";
+            var html = await (rp({
+                url : herokuProxy, 
+                method: 'POST',  
+                headers: {
+                    'Content-Type' : 'application/json',
+                    'Accept' : 'application/json',
+                }, 
+                body: JSON.stringify({url})}));
+
+            var lines = html.split('[');
+            //console.log(selectedDate.substring(0,10) + ": " + (lines.length - 2) + " zacks earnings");
+            lines.forEach(currShareLine => {
+
+                var nStartIndex = currShareLine.indexOf("alt=\\\"");
+                if (nStartIndex != -1) {
+
+                    let nSymbolIndex = nStartIndex + "alt=\\\"".length;
+                    let nEndSymbolIndex = currShareLine.indexOf(" ", nSymbolIndex);
+                    let symbol = currShareLine.substring(nSymbolIndex, nEndSymbolIndex);
+                    let nOffset = currShareLine.indexOf("\"amc\"") != -1 ? 1 : (currShareLine.indexOf("\"bmo\"") != -1 ? 0 : 0);
+                    let nStart = currShareLine.indexOf("<div class");
+                    let estimate = -999;
+
+                    if (nStart != -1)
+                    {
+                        var split = currShareLine.substring(0, nStart).split(',');
+                        var offset = 0;
+
+                        for (var i = 0; i < split.length - 2; i++)
+                            offset += split[i].replace(" ", "").startsWith("\"") ? 0 : 1;
+
+                        var strEstimate = split[4 + offset].substring(2);
+                        strEstimate = strEstimate.substring(0, strEstimate.length - 1);
+                        estimate = parseFloat(strEstimate);
+                        if (estimate == undefined || (estimate != 0 && !estimate) || isNaN(estimate))
+                            estimate = -999;
+                    }
+
+                    if (!containsSymbolsMap[symbol]) {
+                        if ((nOffset == 1 && indicator == "amc") ||
+                            (nOffset == 0 && indicator == "bmo"))
+                            arrToReturn.push({symbol, estimate});
+                    }
+                }
+            });
+        }
+        catch (err) {
+            console.error(err);
+        }
+
+        return arrToReturn;
+    })
+}
